17c504f08f9d68c2afc8c565d6b70b4c02ece48b
Aurélien BOUDOUX <aurelien@boudoux.fr>
2020-08-02 23:29:42 +0200

    mise en place de blazor-State

--- ImmoCalc/Program.cs
+++ ImmoCalc/Program.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Net.Http;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Threading.Tasks;
 using System.Text;
+using BlazorState;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -15,8 +17,23 @@ namespace ImmoCalc {
 			builder.RootComponents.Add<App>("app");
 
 			builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+			ConfigureServices(builder.Services);
 			await builder.Build().RunAsync();
 		}
+
+		public static void ConfigureServices(IServiceCollection aServiceCollection) {
+
+			aServiceCollection.AddBlazorState
+			(
+				(aOptions) =>
+
+					aOptions.Assemblies =
+						new Assembly[]
+						{
+							typeof(Program).GetTypeInfo().Assembly,
+						}
+			);
+		}
 	}
+}
 }
