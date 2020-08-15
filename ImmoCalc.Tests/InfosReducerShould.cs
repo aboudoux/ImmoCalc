59cd106343746357b0f6da24ebb029657d94aa6a
Aurélien BOUDOUX <aurelien@boudoux.fr>
2020-08-15 23:03:53 +0200

    refactor test sur reducer

--- ImmoCalc.Tests/InfosReducerShould.cs
+++ ImmoCalc.Tests/InfosReducerShould.cs
@@ -1,25 +1,42 @@
+using System.Linq;
+using System.Reflection;
 using System.Runtime.InteropServices.ComTypes;
 using System.Threading;
+using BlazorState;
 using FluentAssertions;
 using ImmoCalc.Domain;
 using ImmoCalc.Stores.Infos;
 using ImmoCalc.Tests.Tools;
-using Microsoft.AspNetCore.Components.WebAssembly.Http;
 using Xunit;
 
 namespace ImmoCalc.Tests
 {
 	public class InfosReducerShould
 	{
+		private readonly InfosState _state;
+
+		private void SendAction<TAction>(TAction action) where TAction : IAction
+		{
+			var reducer = new InfosReducer(new TestStore(_state));
+			reducer
+				.GetType()
+				.GetMethods()
+				.First(a => a.Name == "Handle" && a.GetParameters().First().ParameterType == typeof(TAction))
+				.Invoke(reducer, new object?[]{ action, CancellationToken.None});
+		}
+
+		public InfosReducerShould()
+		{
+			_state = new InfosState();
+		}
+
 		[Fact]
 		public void ChangeBuyingInformation()
 		{
-			var state = new InfosState();
-			var reducer = new InfosReducer(new TestStore(state));
-			reducer.Handle(new InfosState.ChangeBuyingPrice(BuyingPrice.From(100000)), CancellationToken.None );
+			SendAction(new InfosState.ChangeBuyingPrice(BuyingPrice.From(100000)));
 
-			state.BuyingPrice.Value.Should().Be(100000);
-			state.NotaryFees.Value.Should().Be(7500);
+			_state.BuyingPrice.Value.Should().Be(100000);
+			_state.NotaryFees.Value.Should().Be(7500);
 		}
 	}
 }
